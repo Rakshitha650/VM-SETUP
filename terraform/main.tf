d6d969783f2a94caf187aae26607ae5e575f0431
techno-376 <rakshitham38@gmail.com>
2025-05-07 20:08:53 +0530

    [task]

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "performance_vm" {
 }
 
 resource "aws_security_group" "perf_vm_sg" {
-  name        = "mosip-k8s-performance-vm256"
+  name        = "mosip-k8s-performance-vm259"
   description = "Allow necessary access"
 
   ingress {
